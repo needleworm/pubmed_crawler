e3183ed6f82094a39fafa706e3e9c34e87b79094
needleworm <bhban@kakao.com>
2020-08-24 14:18:34 +0900

    .

---
+++ chem_crawler.py
@@ -0,0 +1,70 @@
+"""
+Author : Byunghyun Ban
+Last Modification
+2020.08.24.
+https://github.com/needleworm
+halfbottle@sangsang.farm
+"""
+import numpy as np
+import pmcrawl as P
+import itertools
+import pyexcel as px
+
+
+class ChemCrawler:
+    def __init__(self, keyword):
+        self.keyword = keyword
+        self.chem_json_list, self.chem_list, self.name_dict = self.process_pubmed_chem_info(keyword)
+        self.chem_matrix = self.process_matrix()
+
+    def make_xlsx_single_chem(self, outfile=None):
+        if not outfile:
+            outfile = "frequency_" + self.keyword + ".xlsx"
+        header = ["Compound ID", "Name", "Frequency"]
+
+        contents = []
+        contents.append(header)
+        for i in range(len(self.chem_list)):
+            compound_id = self.chem_list[i]
+            name = self.name_dict[compound_id]
+            frequency = self.chem_matrix[i, i]
+            contents.append([compound_id, name, frequency])
+
+        px.save_as(array=contents, dset_file_name=outfile)
+
+        print("Result Saved as an XLSX File")
+
+    def process_matrix(self):
+        num_chem = len(self.chem_list)
+        matrix = np.zeros((num_chem, num_chem), dtype=np.int)
+
+        for chem_json in self.chem_json_list:
+            keys = list(chem_json.keys())
+
+            IDXs = []
+
+            for el in keys:
+                idx = self.chem_list.index(el)
+                matrix[idx, idx] += 1
+                IDXs.append(idx)
+
+            for i, j in itertools.permutations(IDXs, 2):
+                matrix[i, j] += 1
+
+        return matrix
+
+    def process_pubmed_chem_info(self, keyword):
+        chem_json_list = P.crawl_chem_json(keyword)
+        chem_list = []
+        name_dict = {}
+
+        for chem_json in chem_json_list:
+            for chem in chem_json.keys():
+                if chem not in chem_list:
+                    chem_list.append(chem)
+                    name_dict[chem] = chem_json[chem]["substance_name"]
+
+        print("Total Number of Crawled Papers : " + str(chem_json_list))
+        print("Total Number of Chemicals : " + str(chem_list))
+
+        return chem_json_list, chem_list, name_dict
---
+++ pmcrawl.py
@@ -0,0 +1,124 @@
+"""
+Author : Byunghyun Ban
+Last Modification : 2020.08.19.
+halfbottle@sangsang.farm
+https://github.com/needleworm
+"""
+
+from metapub import PubMedFetcher
+
+
+def remove_escape(string):
+    retval = ""
+    for el in string:
+        if el not in "\n\t":
+            retval += el
+        else:
+            retval += " "
+    return retval
+
+
+def crawl_chem_json(keyword, retmax=1000):
+    fetch = PubMedFetcher()
+
+    pmids = fetch.pmids_for_query(keyword, retmax=retmax)
+    print("PMID scan Done!")
+
+    json_dicts = []
+
+    for pmid in pmids:
+        article = fetch.article_by_pmid(pmid)
+        if not article:
+            continue
+
+        chemical = article.chemicals
+        if not chemical:
+            continue
+
+        print(str(chemical) + "\n")
+
+        json_dicts.append(chemical)
+
+    print("Process Done!")
+    return json_dicts
+
+
+def crawl_abstract(keyword, outfile=None, max_iter=1000, has_chem_only=False):
+    fetch = PubMedFetcher()
+
+    pmids = fetch.pmids_for_query(keyword, retmax=max_iter)
+    print("PMID scan Done!")
+
+    if not outfile:
+        outfile = "[Crawling Results]" + keyword + ".tsv"
+
+    o_file = open(outfile, 'w', encoding="utf8")
+
+    header = "PMID\tAuthors\tYear\tTitle\tAbstract\tURL\tCitation\tChemicals\n"
+    o_file.write(header)
+
+    for pmid in pmids:
+        article = fetch.article_by_pmid(pmid)
+        if not article:
+            continue
+
+        authors = article.authors_str
+        if not authors:
+            continue
+        elif "\t" in authors or "\n" in authors:
+            authors = remove_escape(authors)
+
+        year = article.year
+        if not year:
+            continue
+        elif "\t" in year or "\n" in year:
+            year = remove_escape(year)
+
+        title = article.title
+        if not title:
+            continue
+        elif "\t" in title or "\n" in title:
+            title = remove_escape(title)
+
+        abstract = article.abstract
+        if not abstract:
+            continue
+        elif "\t" in abstract or "\n" in abstract:
+            abstract = remove_escape(abstract)
+
+        url = article.url
+        if not url:
+            continue
+        elif "\t" in url or "\n" in url:
+            url = remove_escape(url)
+
+        citation = article.citation
+        if not citation:
+            continue
+        elif "\t" in citation or "\n" in citation:
+            citation = remove_escape(citation)
+
+        chemical = article.chemicals
+        if not chemical:
+            if has_chem_only:
+                continue
+            chemical = "None"
+        else:
+            chemical = str(chemical).replace("\'", "\"")
+            if "\t" in chemical or "\n" in chemical:
+                chemical = remove_escape(chemical)
+
+        print(article.citation + "\n")
+
+        o_file.write(pmid + "\t")
+        o_file.write(authors + "\t")
+        o_file.write(year + "\t")
+        o_file.write(title + "\t")
+        o_file.write(abstract + "\t")
+        o_file.write(url + "\t")
+        o_file.write(citation + "\t")
+        o_file.write(chemical + "\n")
+
+    o_file.close()
+    print("Process Done!")
+    print("Result is saved in <" + outfile + ">.")
