46384f00801b37c69416b0e73fd83b0660242382
needleworm <bhban@kakao.com>
2020-08-26 14:13:43 +0900

    .

--- main.py
+++ main.py
@@ -35,7 +35,6 @@ class Crawl(QThread):
             try:
                 self.chem_json_list, self.chem_list, self.name_dict = self.process_pubmed_chem_info(self.keyword)
             except TimeoutError:
-                print("Please Check Internet Connection! Retrying!")
                 self.textBrowser_value.emit("Please Check Internet Connection! Retrying!")
                 self.chem_json_list, self.chem_list, self.name_dict = self.process_pubmed_chem_info(self.keyword)
         else:
@@ -43,7 +42,6 @@ class Crawl(QThread):
                 self.chem_json_list, self.chem_list, self.name_dict, self.title_list, self.abstract_list = \
                     self.process_pubmed_chem_abstract_info(self.keyword)
             except TimeoutError:
-                print("Please Check Internet Connection! Retrying!")
                 self.textBrowser_value.emit("Please Check Internet Connection! Retrying!")
                 self.chem_json_list, self.chem_list, self.name_dict, self.title_list, self.abstract_list = \
                     self.process_pubmed_chem_abstract_info(self.keyword)
@@ -52,7 +50,6 @@ class Crawl(QThread):
             self.chem_matrix = self.process_matrix()
             self.make_csv_single_chem()
         else:
-            print("No Result Found!")
             self.textBrowser_value.emit("No Result Found!")
 
 
@@ -61,17 +58,13 @@ class Crawl(QThread):
 
         pmids = fetch.pmids_for_query(keyword, retmax=retmax)
 
-        print("Scanning Iteration : " + str(retmax))
         self.textBrowser_value.emit("Scanning Iteration : " + str(retmax))
-        print("Expected Running Time : " + str(retmax * 2) + " seconds.")
         self.textBrowser_value.emit("Expected Running Time : " + str(retmax * 2) + " seconds.")
 
-        print("PMID scan Done!")
         self.textBrowser_value.emit("PMID Scan Done!")
         self.progress_bar_value.emit(self.count)
 
         json_dicts = []
-        print("Crawling Paper Info..")
         self.textBrowser_value.emit("Crawling Paper Info..")
 
         for i in range(len(pmids)):
@@ -83,7 +76,6 @@ class Crawl(QThread):
                 try:
                     article = fetch.article_by_pmid(pmid)
                 except:
-                    print("Error reading " + str(pmid))
                     self.textBrowser_value.emit("Error reading " + str(pmid))
                     continue
 
@@ -95,7 +87,6 @@ class Crawl(QThread):
             except:
                 continue
 
-        print("Process Done!")
         self.textBrowser_value.emit("Progress Done!")
         return json_dicts
 
@@ -129,9 +120,7 @@ class Crawl(QThread):
 
         ofile.close()
 
-        print("Result Saved as a CSV File")
         self.textBrowser_value.emit("Result Saved as a CSV File")
-        print("Filename : " + outfile)
         self.textBrowser_value.emit("Filename : " + outfile)
         self.progress_bar_value.emit(100)
 
@@ -158,7 +147,6 @@ class Crawl(QThread):
 
     def process_pubmed_chem_info(self, keyword):
         chem_json_list = self.crawl_chem_json(keyword, retmax=self.retmax)
-        print("Crawling Done! Processing Output Files...")
         self.textBrowser_value.emit("Crawling Done! Processing Output Files...")
         chem_list = []
         name_dict = {}
@@ -169,8 +157,6 @@ class Crawl(QThread):
                     chem_list.append(chem)
                     name_dict[chem] = chem_json[chem]["substance_name"]
 
-        print("Total Number of Crawled Papers : " + str(len(chem_json_list)))
-        print("Total Number of Chemicals : " + str(len(chem_list)))
         self.textBrowser_value.emit("Total Number of Crawled Papers : " + str(len(chem_json_list)))
         self.textBrowser_value.emit("Total Number of Chemicals : " + str(len(chem_list)))
 
@@ -178,7 +164,6 @@ class Crawl(QThread):
 
     def process_pubmed_chem_abstract_info(self, keyword):
         chem_json_list = self.crawl_chem_abstract(keyword, retmax=self.retmax)
-        print("Crawling Done! Processing Output Files...")
         self.textBrowser_value.emit("Crawling Done! Processing Output Files...")
         chem_list = []
         title_list = []
@@ -196,8 +181,6 @@ class Crawl(QThread):
                     abstract_list.append(chem_json["abstract"])
                     name_dict[key] = chem_json[key]["substance_name"]
 
-        print("Total Number of Crawled Papers : " + str(len(chem_json_list)))
-        print("Total Number of Chemicals : " + str(len(chem_list)))
         self.textBrowser_value.emit("Total Number of Crawled Papers : " + str(len(chem_json_list)))
         self.textBrowser_value.emit("Total Number of Chemicals : " + str(len(chem_list)))
 
@@ -209,16 +192,12 @@ class Crawl(QThread):
 
         pmids = fetch.pmids_for_query(keyword, retmax=retmax)
 
-        print("Scanning Iteration : " + str(retmax))
         self.textBrowser_value.emit("Scanning Iteration : " + str(retmax))
-        print("Expected Running Time : " + str(retmax * 2) + " seconds.")
         self.textBrowser_value.emit("Expected Running Time : " + str(retmax * 2) + " seconds.")
 
-        print("PMID scan Done!")
         self.textBrowser_value.emit("PMID Scan Done!")
 
         json_dicts = []
-        print("Crawling Paper Info..")
         self.textBrowser_value.emit("Crawling Paper Info..")
 
         for i in range(len(pmids)):
@@ -231,7 +210,6 @@ class Crawl(QThread):
                 try:
                     article = fetch.article_by_pmid(pmid)
                 except:
-                    print("Error reading " + str(pmid))
                     self.textBrowser_value.emit("Error reading " + str(pmid))
                     continue
 
@@ -260,7 +238,6 @@ class Crawl(QThread):
             except:
                 continue
 
-        print("Process Done!")
         self.textBrowser_value.emit("Progress Done!")
         return json_dicts
 
